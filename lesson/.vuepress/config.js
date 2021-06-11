8f42a457f2efc3b325501ec69dcbe572cd3d8caa
Zeheng Li <imzehengl@gmail.com>
2021-06-11 14:41:45 -0600

    enable pwa

--- lesson/.vuepress/config.js
+++ lesson/.vuepress/config.js
@@ -7,6 +7,43 @@ module.exports = {
         selector: `div[class*="language-"] pre`,
       },
     ],
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true,
+      },
+    ],
+  ],
+  head: [
+    ["link", { rel: "icon", href: "/logo.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
   ],
   base: process.env.BASE_URL || "/",
   title: "Learn Python",
@@ -18,8 +55,7 @@ module.exports = {
     editLinkText: "Help us improve this page!",
     docsDir: "lesson",
     docsBranch: "main",
-    logo:
-      "https://raw.githubusercontent.com/coding-armadillo/coding-armadillo-logo/main/coding-armadillo-web.png",
+    logo: "/logo.png",
     sidebar: "auto",
     searchPlaceholder: "Search...",
     smoothScroll: true,
