92f70115af7c9b094308515fbce407a9b2872073
Zeheng Li <imzehengl@gmail.com>
2021-06-18 10:27:59 -0600

    auto format

--- scripts/square.py
+++ scripts/square.py
@@ -6,6 +6,6 @@ while line_number < length:
     if line_number == 0 or line_number == length - 1:
         print("*" * length)
     else:
-        print("*" + " "*(length-2) + "*")
+        print("*" + " " * (length - 2) + "*")
 
     line_number += 1
